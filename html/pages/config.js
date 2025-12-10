e3c1dd6c20236e48654e23d167d1a36ab16bccd6
Ryan731-max <tytrenglevades@gmail.com>
2025-12-10 05:27:20 +0000

    Fin du projet

---
+++ html/pages/config.js
@@ -0,0 +1,9 @@
+﻿// config.js
+let cache = null; 
+
+export async function getConfig(name) {
+    if (!cache) {
+        cache = await fetch("/config").then(r => r.json()); // On charge 1 fois
+    }
+    return cache[name]; // On renvoie seulement la variable demandée
+}
---
+++ html/pages/server.js
@@ -0,0 +1,18 @@
+﻿import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+const app = express();
+
+// Sert ton fichier HTML
+app.use(express.static(".")); // sert index.html automatiquement
+
+// Route sécurisée renvoyant uniquement ce que tu choisis
+app.get("/config", (req, res) => {
+    res.json({
+        UNSPLASH_TOKEN: process.env.UNSPLASH_TOKEN,
+        API_OPENAI: process.env.API_OPENAI
+    });
+});
+
+app.listen(3000, () => console.log("Serveur lancé sur http://localhost:3000"));
